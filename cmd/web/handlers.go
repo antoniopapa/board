147b79eceba01d40bfa85240dcd1478dbf60b8d6
Ross Trottier <ross@federated.computer>
2024-06-27 11:18:27 -0600

    Fixed URL bug

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"net/http"
-	"strings"
 )
 
 func (app *application) home(writer http.ResponseWriter, request *http.Request) {
-	domain := request.Host
-	domainPieces := strings.Split(domain, ".")
-	var baseURI string
-	if len(domainPieces) > 1 {
-		baseURI = domainPieces[len(domainPieces) - 2] + domainPieces[len(domainPieces) - 1]
-	} else {
-		baseURI = domain
-	}
+	baseURI := request.Host
 
 	data := templateData{ 
 		AppLinks: getAppLinks(app.tier, baseURI),
