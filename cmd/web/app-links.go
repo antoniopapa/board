4b0658bb69a4f45f7c8faec892e7d4febf49b749
Ross Trottier <ross@federated.computer>
2024-07-01 16:14:06 -0600

    added more space, panel notice, url docs

--- cmd/web/app-links.go
+++ cmd/web/app-links.go
@@ -8,8 +8,11 @@ import (
 type AppLink struct {
 	Title string
 	Url string
+	DocumentationUrl string
 	Image string
 	Description string
+	SpecialNote string
+	LDAP bool
 }
 
 func getAppLinks(tier, baseUri string) []AppLink {
@@ -37,28 +40,35 @@ func getAppLinks(tier, baseUri string) []AppLink {
 func getStarterLinks(baseUri string) []AppLink {
 	return []AppLink {
 		{
-			Title: "User Management",
-			Description: "Create and Manage Users",
+			Title: "Panel",
+			Description: "Create & Manage Users",
 			Image: "/static/img/users.png",
 			Url: "https://panel." + baseUri + "/log_in/",
+			LDAP: true,
+			DocumentationUrl: "https://documentation.federated.computer/docs/core_applications/panel/",
 		},
 		{
 			Title: "Nextcloud",
 			Description: "Email, Files, Documents",
 			Image: "/static/img/nextcloud.png",
 			Url: "https://nextcloud." + baseUri,
+			LDAP: true,
+			DocumentationUrl: "https://docs.nextcloud.com/server/latest/user_manual/en/",
 		},
 		{
 			Title: "Vaultwarden",
-			Description: "Password Management",
+			Description: "Passwords",
 			Image: "/static/img/vaultwarden.png",
 			Url: "https://vaultwarden." + baseUri,
+			LDAP: false,
+			SpecialNote: "Must create your own admin user at setup.",
 		},
 		{
 			Title: "Power DNS",
 			Description: "DNS Management",
 			Image: "/static/img/powerdns.png",
 			Url: "https://powerdns." + baseUri,
+			LDAP: false,
 		},
 	}
 }
@@ -70,12 +80,14 @@ func getCreatorLinks(baseUri string) []AppLink {
 			Description: "Team Chat",
 			Image: "/static/img/element.png",
 			Url: "https://element." + baseUri,
+			LDAP: true,
 		},
 		{
 			Title: "Wordpress",
-			Description: "Your website",
+			Description: "Your Website",
 			Image: "/static/img/wordpress.png",
 			Url: "https://" + baseUri + "/wp-admin",
+			LDAP: false,
 		},
 	}
 
@@ -86,15 +98,17 @@ func getTeamsLinks(baseUri string) []AppLink {
 	teamsLinks := []AppLink {
 		{
 			Title: "Espo CRM",
-			Description: "Customer relationship manager",
+			Description: "Customer Relationship Manager",
 			Image: "/static/img/espo.png",
 			Url: "https://espocrm." + baseUri,
+			LDAP: true,
 		},
 		{
 			Title: "FreeScout",
 			Description: "Customer Help Desk",
 			Image: "/static/img/freescout.png",
 			Url: "https://freescout." + baseUri,
+			LDAP: false,
 		},
 	}
 
@@ -108,36 +122,44 @@ func getEnterpriseLinks(baseUri string) []AppLink {
 			Description: "Video Chat",
 			Image: "/static/img/jitsi.png",
 			Url: "https://jitsi." + baseUri,
+			LDAP: true,
 		},
 		{
 			Title: "Listmonk",
 			Description: "Email Marketing",
 			Image: "/static/img/listmonk.png",
 			Url: "https://listmonk." + baseUri,
+			LDAP: false,
 		},
 		{
 			Title: "Baserow",
 			Description: "Visual Databases",
 			Image: "/static/img/baserow.png",
 			Url: "https://baserow." + baseUri,
+			LDAP: false,
+			DocumentationUrl: "https://baserow.io/user-docs",
 		},
 		{
 			Title: "Bookstack",
 			Description: "Wiki Knowledgebase",
 			Image: "/static/img/bookstack.png",
 			Url: "https://bookstack." + baseUri,
+			LDAP: false,
+			DocumentationUrl: "https://www.bookstackapp.com/docs/",
 		},
 		{
 			Title: "Gitea",
 			Description: "GIT Source Control",
 			Image: "/static/img/gitea.png",
 			Url: "https://gitea." + baseUri,
+			LDAP: false,
 		},
 		{
 			Title: "Castopod",
 			Description: "Podcast Distribution",
 			Image: "/static/img/castopod.png",
 			Url: "https://castopod." + baseUri + "/cp-auth/login",
+			LDAP: false,
 		},	
 	}
 
